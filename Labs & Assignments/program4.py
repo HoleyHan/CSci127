08ccb9f9eb9afd66d0e093c063dae56524681105
HoleyHan <69694472+HoleyHan@users.noreply.github.com>
2024-09-04 16:51:35 -0400

    Upload

---
+++ Labs & Assignments/program4.py
@@ -0,0 +1,8 @@
+#Name: Han Ning Zhang, Han Zhang
+#Email: han.zhang65@myhunter.cuny.edu, han.zhang.med18@gmail.com
+#Date: September 3, 2024
+#This program prints: I love Python! x25 
+
+#each iteration begins on a new line
+for i in range(25):
+    print("I love Python!")
