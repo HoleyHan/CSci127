23fb8051d419ce8f81adf756c8bf2c04585e757c
HoleyHan <69694472+HoleyHan@users.noreply.github.com>
2024-09-17 11:02:46 -0400

    update

--- program1.py
+++
@@ -1,5 +0,0 @@
-#Name: Han Ning Zhang, Han Zhang
-#Email: han.zhang65@myhunter.cuny.edu, han.zhang.med18@gmail.com
-#Date: September 3, 2024
-#This program prints: Hello, World!
-print("Hello, World!")
