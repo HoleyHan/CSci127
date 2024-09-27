9b3142d5916623b8c3e74e0f28f89e35caf35403
HoleyHan <69694472+HoleyHan@users.noreply.github.com>
2024-09-27 15:37:55 -0400

    test

---
+++ Labs & Assignments/lab4/lab1.py
@@ -0,0 +1,22 @@
+# Name:  Han Zhang
+# Email: han.zhang.med18@gmail.com
+# Date:  September 2024
+# Takes elevation data of NYC and displays using the default color map
+
+#Import the libraries for arrays and displaying images:
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Read in the data to an array, called elevations:
+elevations = np.loadtxt('elevationsNYC.txt')
+#Load the array into matplotlib.pyplot:
+plt.imshow(elevations)
+#Display the plot:
+plt.show()
+
+#Unix Notes: 
+# mkdir = make directory
+# cd = change directory
+# pwd = print working directory
+# grep string filename(s) = global regular expression printlook for string in the files
+# whoami
