4c9d6180c93bad7179c172f91bc3873b86955a76
HoleyHan <69694472+HoleyHan@users.noreply.github.com>
2024-10-01 12:50:38 -0400

    Update program14.py

--- Assignments/program14.py
+++ Assignments/program14.py
@@ -6,14 +6,16 @@
 import turtle
 
 # Turtle module GUI
-wn = turtle.Screen()
+#wn = turtle.Screen()
 t = turtle.Turtle()
-t.speed(5)
+#t.speed(5)
 
 # Ask the user for 5 angles and move the turtle accordingly
 for _ in range(5):  # Loop 5 times
     degree = int(input("Please enter an integer degree: "))
     t.left(degree)  # Turn the turtle left by the entered degrees
-    t.forward(100)  # Move the turtle forward by 100 units
+    t.fd(100)  # Move the turtle forward by 100 units
 
-wn.exitonclick()  # Exit when the window is clicked
+#wn.exitonclick()  # Exit when the window is clicked
+
+#test 270, 100, 190, 200, 80
