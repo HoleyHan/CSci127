9b632f3d624d545cd522f23fe5f90ddf0a986fc8
HoleyHan <69694472+HoleyHan@users.noreply.github.com>
2024-10-01 13:07:42 -0400

    s

--- Assignments/program14.py
+++ Assignments/program14.py
@@ -14,13 +14,24 @@ t.speed(5)
 #initial_heading = t.heading()
 #print(f"Initial heading: {initial_heading}")
 
+turnList2 = [72, 72, 72, 72, 72]
+for n in turnList2:
+  t.left(n)
+  t.fd(100)
+  t.write(t.heading())
+
+turnList = [270, 100, 190, 200, 80]
+for n in turnList:
+  t.left(n)
+  t.fd(100)
+  t.write(t.heading())
 
 # Ask the user for 5 angles and move the turtle accordingly
 for _ in range(5):  # Loop 5 times
     degree = int(input("Please enter an integer degree: "))
     t.left(degree)  # Turn the turtle left by the entered degrees
-    print(f"Current heading: {t.heading()}")
     t.fd(100)  # Move the turtle forward by 100 units
+    t.write(t.heading()) # Write the current heading
 
 #wn.exitonclick()  # Exit when the window is clicked
 
